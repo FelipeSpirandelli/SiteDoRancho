92c34631b40e3c8a4f52540914fa3049e01514aa
Felipe Spirandelli <70784142+FelipeSpirandelli@users.noreply.github.com>
2021-05-30 15:51:49 -0300

    feat: make navbar sticky and reduce the buttons to one circle

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react'
 import logo from "../assets/images/gapsj.png"
 import BaseButton from "./button.js"
 import "../CSS/texts.css"
@@ -5,15 +6,36 @@ import "../CSS/navbar.css"
 
 const Navbar = () => {
 
-    return(
-        <div className = "navbar">
-            <div className = "within">
-                <img src = {logo} alt = "Logo GAP-SJ" />
+    const [scrolled, setScrolled] = React.useState(false);
+    const handleScroll = () => {
+        const offset = window.scrollY;
+        if (offset > 50) {
+            setScrolled(true);
+        }
+        else {
+            setScrolled(false);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
+    })
+    let navbarClasses = ['within'];
+    if (scrolled) {
+        navbarClasses.push('scrolled');
+    }else{
+        navbarClasses.push("text-button");
+    }
+
+    return (
+        <div className= "navbar">
+            <div className="within">
+                <img src={logo} alt="Logo GAP-SJ" />
                 <h4> GAP </h4>
             </div>
-            <div className = "within">
-                <BaseButton classname = "text-button" text = "CARDÁPIO" />
-                <BaseButton classname = "text-button" text = "SOBRE NÓS" />
+            <div className="within">
+                <BaseButton classname={navbarClasses.join(" ")} text="CARDÁPIO" />
+                <BaseButton classname="text-button" text="SOBRE NÓS" />
             </div>
         </div>
     )
